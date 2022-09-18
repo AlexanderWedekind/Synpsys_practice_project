8241025de349372bb2d4657ae1087884ef62bffd
Alexander Wedekind <awedwork@gmail.com>
2022-09-18 12:48:06 +0100

    disregards not play again

--- python_projects/play_guess_with_computer.py
+++ python_projects/play_guess_with_computer.py
@@ -7,34 +7,40 @@ print("Hello, let's play a game. \nYou choose a number, and the computer will tr
 input("Press 'enter' when ready")
 print("Great!")
 
-playAgain = "y"
-lowerLimit = 1
-upperLimit = 1000
 
+def askForResponse(guess):
+    response = input(f"Is {guess} your number (y), or is that too high (h), or too low (l)?\n(Press 'y' for yes, 'l' for low, or 'h' for high. then press 'enter')")
+    return response
 
-
-def computerGuesses(lowerLimit, upperLimit):
-    guess = random.randint(lowerLimit, upperLimit)
-    response = input(f"Is {guess} your number (y), or is that too high (h), or too low (l)?\n(Press 'y' for yes, 'l' for low, or 'h' for high")
+def evaluateAnswers(response, guess, lowerLimit, upperLimit):
     if response == "y":
         computerWon()
     if response == "h":
-        computerGuesses(lowerLimit, upperLimit-1)
+        computerGuesses(lowerLimit, guess-1)
     if response == "l":
-        computerGuesses(lowerLimit+1, upperLimit)
+        computerGuesses(guess+1, upperLimit)
     else:
-        response = input(f"Is {guess} your number (y), or is that too high (h), or too low (l)?\n(Press 'y' for yes, 'l' for low, or 'h' for high")      
+        print("Please answer with 'y', 'h' or 'l'.")
+        evaluateAnswers(askForResponse(guess), guess)
+
+def computerGuesses(lowerLimit, upperLimit):
+    guess = random.randint(lowerLimit, upperLimit)
+    
+    evaluateAnswers(askForResponse(guess), guess, lowerLimit, upperLimit)
+    
     
 def computerWon():
     print("Computer won!")
-    askToPlay()
+    playAgain = askToPlay()
     if playAgain == "y":
         print("Let's go!")
-        lowerLimit = 1
-        upperLimit = 1000
-        computerGuesses(lowerLimit, upperLimit)
+        computerGuesses(1, 100)
+    else:
+        print("Thanks for playing, see you next time.")
 
 
 def askToPlay():
-    playAgain = input("Hit 'y' to play again, or any other key to quit")
-    return playAgain
+    yesOrNo = input("Hit 'y'and then 'enter' to play again, or any other key + 'enter' to quit")
+    return yesOrNo
+
+computerGuesses(1, 100)
