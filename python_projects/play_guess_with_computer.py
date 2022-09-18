53cb254d9cffc1fcc7512546cc070641c686e6f7
Alexander Wedekind <awedwork@gmail.com>
2022-09-18 11:47:11 +0100

    'win' function and 'play again prompt' function

---
+++ python_projects/play_guess_with_computer.py
@@ -0,0 +1,31 @@
+from ast import Num
+import numbers
+import random
+from tokenize import Number
+
+print("Hello, let's play a game. \nYou choose a number, and the computer will tru to guess it.")
+input("Press 'enter' when ready")
+print("Great!")
+
+playAgain = "y"
+lowerLimit = 1
+upperLimit = 1000
+
+def computerGuesses(lowerLimit, upperLimit):
+    guess = random.randint(lowerLimit, upperLimit)
+    reponse = input(f"Is {guess} your number (y), or is that too high (h), or too low (l)?\n(Press 'y' for yes, 'l' for low, or 'h' for high")
+    if 
+    
+def computerWon():
+    print("Computer won!")
+    askToPlay()
+    if playAgain == "y":
+        print("Let's go!")
+        lowerLimit = 1
+        upperLimit = 1000
+        computerGuesses(lowerLimit, upperLimit)
+
+
+def askToPlay():
+    playAgain = input("Hit 'y' to play again, or any other key to quit")
+    return playAgain
