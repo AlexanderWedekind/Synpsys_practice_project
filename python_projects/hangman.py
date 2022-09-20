b6ec135766744666f27cb330397bb082be9157e8
Alexander Wedekind <awedwork@gmail.com>
2022-09-20 19:27:15 +0100

    play functions and check next guess function, not yet works

--- python_projects/hangman.py
+++ python_projects/hangman.py
@@ -1,15 +1,24 @@
 import random
 from typing import Literal
 
+
 dictionary = ["able", "brunch", "clear", "drive"]
+
+pastGuesses = ""
+
+guessedLetter = ""
+
+displayWord = ""
+
+lives = 12
+
+def resetLives():
+    lives = 12
+    return lives
 
 def chooseAWord(dictionary):
     chosenword = random.choice(dictionary)
     return chosenword
-
-chosenWord = chooseAWord(dictionary)
-
-print(chosenWord)
 
 def makeDisplayWord(chosenWord):
     displayWord = ""
@@ -19,12 +28,41 @@ def makeDisplayWord(chosenWord):
     
     return displayWord
 
-print(makeDisplayWord(chosenWord))
+
 
 def receiveGuessFromPlayer():
     print('Make another guess!\n(press any key, then hit ENTER):')
     guessedLetter = input()
     return guessedLetter 
+
+def rememberGuess(guessedLetter):
+    pastGuesses = pastGuesses + guessedLetter
+    return pastGuesses
+
+def play(lives, displayWord, pastGuesses):
+    print(f"you have {lives} lives left.\nyour word so far: {displayWord}\nletters used so far: {pastGuesses}")
+    nextGuess = input("guess a letter:\n(any key + ENTER)")
+    lives = lives + 1
+    return nextGuess
+
+def checkNextGuess(nextGuess):
+    for letter in chosenWord:
+        if letter == nextGuess:
+            displayWord[chosenWord.index(nextGuess)]
+
+def askToPlay():
+    answer = input("Do you want to play hangman?\nchoose (y)es, or (n)o, then press ENTER")
+    if answer == "y":
+       play()
+    
+play()
+
+
+chosenWord = chooseAWord(dictionary)
+
+print(chosenWord)
+
+print(makeDisplayWord(chosenWord))
 
 guessedLetter = receiveGuessFromPlayer()
 
