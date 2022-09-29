d52984427dc0cd68f074d24b175913728adf2228
vileider <andrewbagio@yahoo.com>
2022-09-30 00:04:46 +0100

    cpp rock paper scissors solution Co-authored-by: alexander wedekind <AlexanderWedekind@users.noreply.github.com>

---
+++ cplus_projects/bogdan_learning/2_lesson.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    double weight = 5;
+    double weightOnMars = weight * 0.38;
+    std::cout << "enter your weight" << weight;
+    std::cin >> weight;
+    std::cin;
+    return 0;
+}
